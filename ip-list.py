e4433b875f84ef68fc71af989e721687d27b77f3
SemchenkoSergey <cem4enko.cepreu@gmail.com>
2018-08-21 16:14:00 +0300

    Формирование списка дслам из отчета Аргус

--- ip-list.py
+++ ip-list.py
@@ -1,18 +1,61 @@
 #!/usr/bin/env python3
 # coding: utf8
 
+import csv
 import os
-from resources import Settings
+import pickle
+
+
+
+def generate_dslams():
+    dslams = []
+    with open('список.csv',  encoding='windows-1251') as f:
+        reader = csv.reader(f, delimiter=';')
+        for row in reader:
+            if len(row) < 35:
+                continue
+            elif 'Российская Федерация Гос-во' not in row[2]:
+                continue
+            model = row[4].replace('=', '').replace('"', '')
+            ip = row[6].replace('=', '').replace('"', '')
+            if model == 'Huawei MA 5616':
+                dslams.append((ip, '5616'))
+            elif model == 'Huawei MA 5600':
+                dslams.append((ip, '5600'))
+    with open('dslams', 'bw') as file_dump:
+            pickle.dump(dslams, file_dump)
+
+def load_dslams():
+    try:
+        with open('dslams', 'br') as file_load:
+            return pickle.load(file_load)
+    except:
+        return []
+
+def get_ip_list(dslams):
+    result = ''
+    number = 1
+    for dslam in dslams:
+        if len(result + dslam[0]) < 239:
+            result += dslam[0] + ';'
+        else:
+            print('{} строка для формирования отчета: {}\n'.format(number, result))
+            result = dslam[0] + ';'
+            number += 1
+    print('{} строка для формирования отчета: {}\n'.format(number, result))
+    
+
+
+
 
 def main():
-    out = ''
-    for host in Settings.hosts:
-        if len(out + host[0]) < 239:
-            out += host[0] + ';'
-        else:
-            print('Строка для формирования отчета: {}\n'.format(out))
-            out = host[0] + ';'
-    print('Строка для формирования отчета: {}'.format(out))
+    generate_dslams()
+    dslams = load_dslams()
+    for dslam in dslams:
+        print(dslam)
+    print()
+    get_ip_list(dslams)
+
 
 if __name__ == '__main__':
     cur_dir = os.sep.join(os.path.abspath(__file__).split(os.sep)[:-1])
