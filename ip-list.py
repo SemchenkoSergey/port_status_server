5c4f2ea11639c5d88ec7d40d53a2e266e62c5584
SemchenkoSergey <cem4enko.cepreu@gmail.com>
2018-08-28 21:08:59 +0300

    Убрал распечатку ip, с новым отчетом это больше не нужно

--- ip-list.py
+++ ip-list.py
@@ -12,7 +12,6 @@ def generate_dslams(files):
     for file in files:
         if file.split('.')[-1] != 'csv':
             continue
-        cur_dslams = []
         with open(file,  encoding='windows-1251') as f:
             print('Обработка {}'.format(file))
             reader = csv.reader(f, delimiter=';')
@@ -28,13 +27,12 @@ def generate_dslams(files):
                 if ip == '':
                     continue
                 if model == 'Huawei MA 5616':
-                    cur_dslams.append((ip, '5616'))
+                    dslams.append((ip, '5616'))
                 elif model == 'Huawei MA 5600':
-                    cur_dslams.append((ip, '5600'))
-        print_ip_list(file, cur_dslams)
-        dslams.extend(cur_dslams)
+                    dslams.append((ip, '5600'))
     with open('resources{}dslams.db'.format(os.sep), 'bw') as file_dump:
             pickle.dump(dslams, file_dump)
+    return len(dslams)
 
 
 def load_dslams():
@@ -43,22 +41,6 @@ def load_dslams():
             return pickle.load(file_load)
     except:
         return []
-
-
-def print_ip_list(file, dslams):
-    if len(dslams) == 0:
-        return
-    print('\r\nНаборы ip для формирования отчетов из файла {}:'.format(file))
-    result = ''
-    number = 1
-    for dslam in dslams:
-        if len(result + dslam[0]) < 239:
-            result += dslam[0] + ';'
-        else:
-            print('{}: {}'.format(number, result))
-            result = dslam[0] + ';'
-            number += 1
-    print('{}: {}\r\n'.format(number, result))
 
 
 def delete_files(files):
@@ -72,13 +54,14 @@ def main():
     if len(files) == 0:
         return    
     # Обработка файлов
-    print("Обработка файлов: {}\r\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M')))
-    generate_dslams(files)
-    dslams = load_dslams()
+    print("Запуск программы: {}\n".format(datetime.datetime.now().strftime('%Y-%m-%d %H:%M')))
+    count = generate_dslams(files)
+    print('\nОбнаружено DSLAM: {}'.format(count))
+    #dslams = load_dslams()
     #for idx, dslam in enumerate(dslams):
         #print('{}: {}'.format(idx, dslam))
     #print()
-    #delete_files(files)
+    delete_files(files)
 
 
 if __name__ == '__main__':
